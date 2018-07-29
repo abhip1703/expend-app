7f0a39ae26fcb366645345b1e3e0b66dca1d1d9a
Abhishek Paradkar <abhishek@Abhisheks-MacBook-Air.local>
2018-07-29 21:59:52 +0530

    change minor

--- public/src/components/ExpenseListItem.js
+++ public/src/components/ExpenseListItem.js
@@ -7,7 +7,7 @@ const ExpenseListItem = ({id, description, amount, createdAt}) => (
     <Link className="list-item" to={`/edit/${id}`}>
     <div >
     <h3 className="list-item__title">{description}</h3>
-    <span className="list-item__subtitle">{moment(createdAt).format('MMMM Do, YYYY ')}</span>
+    <span className="list-item__subtitle">{moment(createdAt).format('Do MMMM, YYYY ')}</span>
     </div>
     <h3 className="list-item__data">
     ₹{numeral(amount/100).format('0,0.00')}
